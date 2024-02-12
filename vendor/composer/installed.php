69c096afda63bb1a860e0d17f7023bde24728f0a
shazzad <hello@shazzad.me>
2024-02-12 19:57:56 +0600

    Added vendor in gitignore for build file generation

---
+++ vendor/composer/installed.php
@@ -0,0 +1,34 @@
+<?php return array(
+    'root' => array(
+        'name' => 'w4devinc/w4-post-list',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '5d298488f5c313e8c1c4ab80e0b7775c04a52c09',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'appsero/client' => array(
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'reference' => '27613f79a6e178e75abd8a3b92c9db0f992c96cd',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../appsero/client',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+        'w4devinc/w4-post-list' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '5d298488f5c313e8c1c4ab80e0b7775c04a52c09',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
